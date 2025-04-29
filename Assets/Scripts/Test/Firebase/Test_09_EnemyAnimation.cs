a6c126621ad9afe6d8c4cf0fe8064bcd091cc279
LeeJaeUng <lsam3333333@gmail.com>
2025-04-29 15:59:11 +0900

    2025.04.29 적 애니메이터 콘트롤러 통합
    * 기존 AC활용하여 defaultAC 제작 후 다른 Enemy에 맞게 AOC 사용
    
    * Animation - Fishs - _Default 폴더

--- Assets/Scripts/Test/Firebase/Test_09_EnemyAnimation.cs
+++ Assets/Scripts/Test/Firebase/Test_09_EnemyAnimation.cs
@@ -5,23 +5,33 @@ public class Test_09_EnemyAnimation : TestBase
 {
     public Animator animator;
 
+    public string animName1 = "IsWalk";
+    public string animName2 = "IsAttack";
+    public string animName3 = "IsHurt";
+    public string animName4 = "IsDead";
+    public bool isTrue = true;
+
     public override void Test3(InputAction.CallbackContext context)
     {
-        animator.SetTrigger("Attack_1");
+        animator.SetBool(animName1, isTrue);
+        isTrue = !isTrue;
     }
 
     public override void Test4(InputAction.CallbackContext context)
     {
-        animator.SetTrigger("Attack_2");
+        animator.SetBool(animName2, isTrue);
+        isTrue = !isTrue;
     }
 
     public override void Test5(InputAction.CallbackContext context)
     {
-        animator.SetTrigger("Teleport");
+        animator.SetBool(animName3, isTrue);
+        isTrue = !isTrue;
     }
 
     public override void Test6(InputAction.CallbackContext context)
     {
-        animator.SetTrigger("PhaseChange");
+        animator.SetBool(animName4, isTrue);
+        isTrue = !isTrue;
     }
 }
